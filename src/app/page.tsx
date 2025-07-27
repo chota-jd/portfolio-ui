41776475dc2da3440b5c0ed338e4e3771fed39d6
Chirag Prajapati <chotajd4444@gmail.com>
2025-07-27 13:39:09 +0530

    fix: deleted all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Skills from '@/components/Skills';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
-import Footer from '@/components/Footer';
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+   hello
     </main>
   );
 }
